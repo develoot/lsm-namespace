2da95d4a8647b504a2c3ddb8c97405b542ab0534
roonm813 <roonm813@naver.com>
2019-12-08 16:25:04 +0900

    Add lmsns procfs
    Make file in procfs named 'lsmns'.
    Add parse_lsmns_procfs() to parsing lsm module name and get flag.

---
+++ fs/proc/lsmns.c
@@ -0,0 +1,96 @@
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/parser.h>
+
+#define LEN 1024
+#define LSM_NS_SELINUX		0x00000001
+#define LSM_NS_APPARMOR		0x00000002
+#define LSM_NS_TOMOYO		0x00000004
+
+static const enum {
+	Lsmns_selinux,
+	Lsmns_apparmor,
+	Lsmns_tomoyo,
+	Lsmns_err
+};
+
+const match_table_t tokens = {
+	{Lsmns_selinux, "selinux"},
+	{Lsmns_apparmor, "apparmor"},
+	{Lsmns_tomoyo, "tomoyo"},
+	{Lsmns_err, NULL}
+};
+
+struct proc_dir_entry *proc_lsm;
+char lsm_buff[LEN];
+
+int parse_lsmns_procfs(void)
+{
+	int types = 0;
+	substring_t args[MAX_OPT_ARGS];
+	int token;
+	int str_len = strlen(lsm_buff);
+	if(str_len == 0)
+		return types;
+	char *str = kmalloc(str_len, GFP_KERNEL);
+	strlcpy(str, lsm_buff, str_len);
+
+	int i;
+	for(i = 0; i < str_len; i++){
+		if(str[i] == ' ' || str[i] =='.' || str[i] == '\n' || str[i] == '\r')
+			str[i] = ',';
+	}
+
+	char *tmp = &str[0];
+	char *p;
+        while((p = strsep(&tmp, ",")) != NULL){
+                if (!*p)
+                        continue;
+                token = match_token(p, tokens, args);
+                switch (token) {
+                case Lsmns_selinux:
+                        types |= LSM_NS_SELINUX;
+                        break;
+                case Lsmns_apparmor:
+                        types |= LSM_NS_APPARMOR;
+                        break;
+                case Lsmns_tomoyo:
+                        types |= LSM_NS_TOMOYO;
+                        break;
+                }
+        }
+	return types;
+}
+
+static ssize_t lsmns_read(struct file* fp, char __user *user_buff,
+               size_t count, loff_t *position)
+{
+        printk(KERN_INFO "read_called\n");
+        return simple_read_from_buffer(user_buff, count, position, lsm_buff, LEN);
+}
+
+static ssize_t lsmns_write(struct file* fp, const char __user *user_buff,
+                size_t count, loff_t *position)
+{
+        printk(KERN_INFO "write called\n");
+        if(count > LEN)
+                return -EINVAL;
+        memset(lsm_buff, 0, LEN);
+        return simple_write_to_buffer(lsm_buff, LEN, position, user_buff, count);
+}
+
+struct file_operations proc_fops = {
+        .read = lsmns_read,
+        .write = lsmns_write,
+};
+
+static int __init proc_lsmns_init(void)
+{
+	proc_create_data("lsmns", 0664, NULL, &proc_fops, "lsmns");
+	return 0;
+}
+
+fs_initcall(proc_lsmns_init);
